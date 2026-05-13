8661879c08e5419836244092b9ee031ca9b48bee
Lahari Sanka <laharisanka@gmail.com>
2026-05-13 22:09:58 +1000

    made buttons and their design + interaction using js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 console.log("JS Connected");
 
-const buttons = document.querySelectorAll(".add-button"); // selects all buttons
+const cart = document.getElementById("cart"); //selects the cart from html using its ID, we can add new items from this
+
+const buttons = document.querySelectorAll(".add-btn"); // selects all buttons
 console.log(buttons); // returns a list of all buttons
 
 buttons.forEach(function(button) { //loops through each button in the list
     button.addEventListener("click", function() { // adding an event listener so that when the button is clicked, the function runs
+        const product = button.parentElement // finds the product card that the button belongs to
+        const productName = product.querySelector("h3").textContent // gets the product name from the h3 inside that card
         const item = document.createElement("li"); //creates a new list item (element, not yet visible but its just been created)
-        item.textContent = "Item added"; // adds text to that specific item (list element)
+        item.textContent = productName; // product name gets put inside the cart
 
-        cart.appendChild(item); //adds that item to the cart as a list item, this will be visible on the page now
-        console.log("Clicked") // to confirm that the button has actually been clicked
+        cart.appendChild(item); //adds item to the cart, this will be visible on the page now
+        console.log(productName + "added to cart") // confirms the item has been added to cart
     });
 });
 
-const cart = document.getElementById("cart"); //selects the cart from html using its ID, we can add new items from this
+const clearBtn = document.getElementById("clear-cart"); // selects the clear cart button
+
+clearBtn.addEventListener("click", function()) { //when the button is clicked, the function runs and everything inside the cart is cleared
+    cart.innerHTML = ""; // to clear all items in cart
+    console.log("Cart cleared!");
+}
